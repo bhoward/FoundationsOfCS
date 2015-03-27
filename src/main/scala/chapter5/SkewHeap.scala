0141e7c9d8762f2d178508e3d50f9f8f0accbdfc
Brian Howard <bhoward@depauw.edu>
2015-03-27 10:16:25 -0400

    Added Skew Heaps.

---
+++ src/main/scala/chapter5/SkewHeap.scala
@@ -0,0 +1,72 @@
+package chapter5
+
+import java.util.Scanner
+
+object SkewHeap {
+  sealed trait Heap
+  case object Empty extends Heap
+  case class Node(left: Heap, value: Int, right: Heap) extends Heap
+  
+  private def merge(h1: Heap, h2: Heap): Heap = (h1, h2) match {
+    case (Empty, _) => h2
+    case (_, Empty) => h1
+    case (Node(l1, v1, r1), Node(l2, v2, r2)) =>
+      if (v1 <= v2) {
+        Node(r1, v1, merge(l1, h2))
+      } else {
+        Node(r2, v2, merge(l2, h1))
+      }
+  }
+
+  def insert(h: Heap, x: Int): Heap = {
+    merge(h, Node(Empty, x, Empty))
+  }
+  
+  
+  def insertAll(h: Heap, xs: List[Int]): Heap = xs match {
+    case Nil => h
+    case head :: tail => insertAll(insert(h, head), tail)
+  }
+
+  def findMin(h: Heap): Option[Int] = h match {
+    case Empty => None
+    case Node(_, m, _) => Option(m)
+  }
+
+  def deleteMin(h: Heap): Heap = h match {
+    case Empty => Empty
+    case Node(l, _, r) => merge(l, r)
+  }
+
+  def removeAll(h: Heap): List[Int] = findMin(h) match {
+    case None => Nil
+    case Some(m) => {
+      m :: removeAll(deleteMin(h))
+    }
+  }
+
+  def heapSort(xs: List[Int]): List[Int] = {
+    removeAll(insertAll(Empty, xs))
+  }
+
+  def main(args: Array[String]): Unit = {
+    val in = new Scanner(System.in)
+    val list = makeList(in)
+    printList(heapSort(list))
+  }
+
+  def makeList(in: Scanner): List[Int] = {
+    if (!in.hasNextInt) {
+      Nil
+    } else {
+      val x = in.nextInt()
+      x :: makeList(in)
+    }
+  }
+
+  def printList(list: List[Int]): Unit = {
+    for (current <- list) {
+      println(current)
+    }
+  }
+}
---
+++ src/test/scala/chapter5/SkewHeapTest.scala
@@ -0,0 +1,77 @@
+package chapter5
+
+import util.TestSpec
+import org.scalacheck.Arbitrary
+import org.scalacheck.Arbitrary.arbitrary
+
+class SkewHeapTest extends TestSpec {
+  import SkewHeap._
+  
+  implicit def arbHeap: Arbitrary[Heap] = Arbitrary {
+    for (xs <- arbitrary[List[Int]]) yield { insertAll(Empty, xs) }
+  }
+  
+  "Skew Heap insert" should "contain inserted elements" in {
+    forAll { (h: Heap, x: Int) =>
+      removeAll(insert(h, x)).contains(x) should be (true)
+    }
+  }
+
+  "Skew Heap insertAll" should "contain inserted elements" in {
+    forAll { xs: List[Int] =>
+      val a = removeAll(insertAll(Empty, xs))
+      a.size should equal(xs.size)
+      xs.forall(x => a.contains(x)) should be (true)
+      a.forall(x => xs.contains(x)) should be (true)
+    }
+  }
+
+  "Skew Heap deleteMin" should "remove the smallest element" in {
+    forAll { h: Heap =>
+      val m1 = findMin(h)
+      val m2 = findMin(deleteMin(h))
+      
+      if (m1.isEmpty) {
+        m2 shouldBe empty
+      } else {
+        if (m2.nonEmpty) {
+          m1.get should be <= (m2.get)
+        }
+      }
+    }
+  }
+
+  "Skew Heap removeAll" should "be sorted" in {
+    forAll { h: Heap =>
+      val a = removeAll(h)
+      a should equal(a.sorted)
+    }
+  }
+
+  "Skew Heap Sort" should "sort an empty list" in {
+    heapSort(Nil) should equal (Nil)
+  }
+  
+  it should "sort a sample list" in {
+    val list = List(3, 14, 15, 92, 65, 35, 89, 79, 32, 38, 46, 26, 43, 38, 32, 79, 50, 28, 84, 19, 71, 69, 39, 93, 75, 10)
+    heapSort(list) should equal (list.sorted)
+  }
+  
+  it should "sort a sorted list" in {
+    val list = List.iterate(1, 100)(n => n + 1)
+    heapSort(list) should equal (list)
+  }
+  
+  it should "sort a reversed list" in {
+    val list = List.iterate(100, 100)(n => n - 1)
+    val expected = List.iterate(1, 100)(n => n + 1)
+    heapSort(list) should equal (expected)
+  }
+  
+  it should "sort random lists" in {
+    forAll { list: List[Int] =>
+      heapSort(list) should equal (list.sorted)
+    }
+  }
+
+}
