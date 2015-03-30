6e478590b2ac7bcc03ffa1e04f493065e4e25b30
Brian Howard <bhoward@depauw.edu>
2015-03-29 22:46:27 -0400

    Added a scalameter micro-benchmark

---
+++ src/test/scala/chapter2/SortBenchmark.scala
@@ -0,0 +1,73 @@
+package chapter2
+
+import org.scalameter.api._
+import scala.util.Random
+
+object SortBenchmark extends PerformanceTest.OfflineReport {
+  val rand = new Random(0)
+  val sizes = Gen.range("size")(1000, 10000, 1000)
+
+  val sortedArrays = for (size <- sizes) yield Array.range(1, size)
+  val reverseArrays = for (size <- sizes) yield Array.range(size, 1, -1)
+  val randomArrays = for (size <- sizes) yield Array.fill(size)(rand.nextInt)
+
+  val sortedLists = sortedArrays map (_.toList)
+  val reverseLists = reverseArrays map (_.toList)
+  val randomLists = randomArrays map (_.toList)
+
+  performance of "Sorting" config (verbose -> false) in {
+    performance of "Sorted Arrays" in {
+      measure method "Selection Sort" in {
+        using(sortedArrays) in {
+          A => IterativeSelectionSort.selectionSort(A)
+        }
+      }
+      measure method "Recursive Selection Sort" in {
+        using(sortedArrays) in {
+          A => RecursiveSelectionSort.recSelectionSort(A)
+        }
+      }
+      measure method "Merge Sort" in {
+        using(sortedLists) in {
+          L => MergeSortImmutableList.mergeSort(L)
+        }
+      }
+    }
+
+    performance of "Reverse Arrays" in {
+      measure method "Selection Sort" in {
+        using(reverseArrays) in {
+          A => IterativeSelectionSort.selectionSort(A)
+        }
+      }
+      measure method "Recursive Selection Sort" in {
+        using(reverseArrays) in {
+          A => RecursiveSelectionSort.recSelectionSort(A)
+        }
+      }
+      measure method "Merge Sort" in {
+        using(reverseLists) in {
+          L => MergeSortImmutableList.mergeSort(L)
+        }
+      }
+    }
+
+    performance of "Random Arrays" in {
+      measure method "Selection Sort" in {
+        using(randomArrays) in {
+          A => IterativeSelectionSort.selectionSort(A)
+        }
+      }
+      measure method "Recursive Selection Sort" in {
+        using(randomArrays) in {
+          A => RecursiveSelectionSort.recSelectionSort(A)
+        }
+      }
+      measure method "Merge Sort" in {
+        using(randomLists) in {
+          L => MergeSortImmutableList.mergeSort(L)
+        }
+      }
+    }
+  }
+}
