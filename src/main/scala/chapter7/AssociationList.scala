8490bb418071e49220565544151dfd273522b038
Brian Howard <bhoward@depauw.edu>
2015-05-02 13:06:29 -0400

    Finished Chapter 7

--- src/main/scala/chapter7/AssociationList.scala
+++ src/main/scala/chapter7/AssociationList.scala
@@ -4,33 +4,39 @@ package chapter7
 object AssociationList {
   type AList[Key, Value] = List[(Key, Value)]
 
-  def insert[Key, Value](key: Key, value: Value, alist: AList[Key, Value]): AList[Key, Value] = alist match {
-    case Nil => (key, value) :: Nil
-    case (k, v) :: tail =>
-      if (k == key) { // update existing pair
-        (key, value) :: tail
-      } else {
-        (k, v) :: insert(key, value, tail)
-      }
-  }
+  def insert[Key, Value](key: Key, value: Value,
+                         alist: AList[Key, Value]): AList[Key, Value] =
+    alist match {
+      case Nil => (key, value) :: Nil
+      case (k, v) :: tail =>
+        if (k == key) { // update existing pair
+          (key, value) :: tail
+        } else {
+          (k, v) :: insert(key, value, tail)
+        }
+    }
 
-  def lookup[Key, Value](key: Key, alist: AList[Key, Value]): Option[Value] = alist match {
-    case Nil => None
-    case (k, v) :: tail =>
-      if (k == key) {
-        Option(v)
-      } else {
-        lookup(key, tail)
-      }
-  }
+  def lookup[Key, Value](key: Key,
+                         alist: AList[Key, Value]): Option[Value] =
+    alist match {
+      case Nil => None
+      case (k, v) :: tail =>
+        if (k == key) {
+          Option(v)
+        } else {
+          lookup(key, tail)
+        }
+    }
 
-  def delete[Key, Value](key: Key, alist: AList[Key, Value]): AList[Key, Value] = alist match {
-    case Nil => Nil
-    case (k, v) :: tail =>
-      if (k == key) {
-        tail
-      } else {
-        (k, v) :: delete(key, tail)
-      }
-  }
+  def delete[Key, Value](key: Key,
+                         alist: AList[Key, Value]): AList[Key, Value] =
+    alist match {
+      case Nil => Nil
+      case (k, v) :: tail =>
+        if (k == key) {
+          tail
+        } else {
+          (k, v) :: delete(key, tail)
+        }
+    }
 }
--- src/main/scala/chapter7/ListRelation.scala
+++ src/main/scala/chapter7/ListRelation.scala
@@ -4,33 +4,36 @@ package chapter7
 object ListRelation {
   type REL[Dom, Ran] = List[(Dom, Ran)]
 
-  def insert[Dom, Ran](dom: Dom, ran: Ran, rel: REL[Dom, Ran]): REL[Dom, Ran] = rel match {
-    case Nil => (dom, ran) :: Nil
-    case (d, r) :: tail =>
-      if (d == dom && r == ran) {
-        rel
-      } else {
-        (d, r) :: insert(dom, ran, tail)
-      }
-  }
+  def insert[Dom, Ran](dom: Dom, ran: Ran, rel: REL[Dom, Ran]): REL[Dom, Ran] =
+    rel match {
+      case Nil => (dom, ran) :: Nil
+      case (d, r) :: tail =>
+        if (d == dom && r == ran) {
+          rel
+        } else {
+          (d, r) :: insert(dom, ran, tail)
+        }
+    }
 
-  def lookup[Dom, Ran](dom: Dom, rel: REL[Dom, Ran]): List[Ran] = rel match {
-    case Nil => Nil
-    case (d, r) :: tail =>
-      if (d == dom) {
-        r :: lookup(dom, tail)
-      } else {
-        lookup(dom, tail)
-      }
-  }
+  def lookup[Dom, Ran](dom: Dom, rel: REL[Dom, Ran]): List[Ran] =
+    rel match {
+      case Nil => Nil
+      case (d, r) :: tail =>
+        if (d == dom) {
+          r :: lookup(dom, tail)
+        } else {
+          lookup(dom, tail)
+        }
+    }
 
-  def delete[Dom, Ran](dom: Dom, ran: Ran, rel: REL[Dom, Ran]): REL[Dom, Ran] = rel match {
-    case Nil => Nil
-    case (d, r) :: tail =>
-      if (d == dom && r == ran) {
-        tail
-      } else {
-        (d, r) :: delete(dom, ran, tail)
-      }
-  }
+  def delete[Dom, Ran](dom: Dom, ran: Ran, rel: REL[Dom, Ran]): REL[Dom, Ran] =
+    rel match {
+      case Nil => Nil
+      case (d, r) :: tail =>
+        if (d == dom && r == ran) {
+          tail
+        } else {
+          (d, r) :: delete(dom, ran, tail)
+        }
+    }
 }
